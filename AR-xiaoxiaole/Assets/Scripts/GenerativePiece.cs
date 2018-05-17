e8a7ad5dfc9e2f226a4307d3de90d21d6ad4a72f
bilibiliChangKai <3254266353@qq.com>
2018-05-17 23:42:48 +0800

    finish xiaoxiaole

---
+++ AR-xiaoxiaole/Assets/Scripts/GenerativePiece.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerativePiece : MonoBehaviour {
+
+    public AnimationClip generateAnimation;
+
+    protected GamePiece piece;
+
+    void Awake()
+    {
+        piece = GetComponent<GamePiece>();
+        //this.GetComponent<Material>().color = new Color(1, 1, 1, 0);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public virtual void Generate()
+    {
+        StartCoroutine(GenerateCoroutine());
+    }
+
+    private IEnumerator GenerateCoroutine()
+    {
+        Animator animator = GetComponent<Animator>();
+
+        if (animator)
+        {
+            //print("enter");
+            //print(this.transform.position);
+            animator.Play(generateAnimation.name);
+
+            yield return new WaitForSeconds(generateAnimation.length);
+
+            Destroy(this);
+        }
+    }
+}
---
+++ AR-xiaoxiaole/Assets/Scripts/Level.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level : MonoBehaviour {
+	public Grid grid;
+	public HUD hud;
+
+	protected int currentSoldier = 0;
+
+	// Use this for initialization
+	void Start () {
+		hud.SetSoldier (currentSoldier);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public virtual void GameEnd()
+	{
+		grid.GameOver ();
+		StartCoroutine (WaitForGridFill ());
+	}
+
+	public virtual void OnMove()
+	{
+
+	}
+
+	public virtual void OnPieceClicked(GamePiece piece)
+	{
+        currentSoldier++;
+		hud.SetSoldier(currentSoldier);
+	}
+
+    public virtual void OnPieceCleared(GamePiece piece)
+    {
+    }
+
+    protected virtual IEnumerator WaitForGridFill()
+	{
+		while (grid.IsFilling) {
+			yield return 0;
+		}
+
+		hud.OnGameEnd (currentSoldier);
+	}
+}
